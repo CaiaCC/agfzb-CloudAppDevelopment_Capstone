876ba354c9fdee933d28be5e2b0a038b2e5ea8f6
CaiaCC <caiachuang@gmail.com>
2024-01-18 13:13:46 -0500

    created post_request method

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -23,8 +23,15 @@ def get_request(url, **kwargs):
     return json_data
 
 
-# Create a `post_request` to make HTTP POST requests
-# e.g., response = requests.post(url, params=kwargs, json=payload)
+def post_request(url, json_payload, **kwargs):
+    try:
+        response = requests.post(url, params=kwargs, json=payload)
+        status_code = response.status_code
+        print("With status {} ".format(status_code))
+        json_data = json.loads(response.text)
+    except Exception as error:
+        print("Network exception occurred", error)
+    return json_data
 
 
 def get_dealers_from_cf(url, **kwargs):
